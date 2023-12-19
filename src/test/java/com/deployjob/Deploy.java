b53d1a9a6ea3f9dc9c26f23fdb694a3f0bd0e881
Mendepoojitha123 <153059453+Mendepoojitha123@users.noreply.github.com>
2023-12-19 12:17:32 +0530

    Add files via upload
    adding deployjob

---
+++ src/test/java/com/deployjob/Deploy.java
@@ -0,0 +1,10 @@
+package com.deployjob;
+
+public class Deploy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("HI IAM DEPLOY TEAM THIS MY DEPLOYJOB ");
+	}
+
+}
